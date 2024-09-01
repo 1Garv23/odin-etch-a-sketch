1aa4e483e92ec205bd9ef939b6bf500b48c92fdb
1Garv23 <1garv23@gmail.com>
2024-09-02 01:02:22 +0530

    shading functionality added

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let sizeButton=document.querySelector(".size-input");
 let sizeInput=document.querySelector(".number-input");
-let sketchBoard=document.querySelector(".sketch-board")
+let sketchBoard=document.querySelector("#canvas");
+
 
 sizeButton.addEventListener("click", ()=>{createNewBoard()});
-
 function removeOldBoard(){
     while(sketchBoard.firstChild){
         sketchBoard.lastChild.remove();
@@ -12,17 +12,27 @@ function removeOldBoard(){
 function createNewBoard(){
     removeOldBoard();
     let numberOfPixels=parseInt(sizeInput.value);
+    if(numberOfPixels>64){
+        numberOfPixels=64;
+    }
     let pixelSize=960/numberOfPixels;
-    console.log(pixelSize);
     for(let i=0; i<numberOfPixels*numberOfPixels; i++){
         let pixel=document.createElement("div");
         pixel.style.height=pixelSize+"px";
         pixel.style.width=pixelSize+"px";
         pixel.style["box-sizing"]="border-box";
-        pixel.style.border="1px solid red";
+        pixel.style.border="1px solid black";
         sketchBoard.appendChild(pixel);
-        
+        pixel.style["opacity"]=0;
+        pixel.style["background-color"]="black";
+        pixel.addEventListener("mouseenter", (event)=>{colorPixel(event)});
     }
-    // let allPixels=document.querySelectorAll(".sketchBoard *");
-
+}
+function colorPixel(event){
+    let targetPixel=event.target;
+    let currentOpacity=parseFloat(targetPixel.style["opacity"])+0.1;
+    if(currentOpacity>1){
+        return;
+    }
+    targetPixel.style["opacity"]=`${currentOpacity}`;
 }
